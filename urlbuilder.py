7714961ed00e2e6229f2f71df9a4c1e2b023ee43
Matheus Carvalho <matcarvalhosantos@outlook.com>
2021-06-29 19:21:57 -0300

    🆙 Updated search_endpoint() to improve readability

--- urlbuilder.py
+++ urlbuilder.py
@@ -1,28 +1,38 @@
 
-# THIS FILE IS RESPONSABLE FOR BUILDING DYNAMIC URLS
+# THIS FILE IS RESPONSIBLE FOR THE CONSTRUCTION OF DYNAMIC URLS
 
-def search_endpoint(keywords:str, allowed_types:list, 
-filters:dict, market:str, limit:int, offset:int):
-    endpoint = 'https://api.spotify.com/v1/search?'
+
+# PREFIXES
+QUERRY_PREFIX = 'q='
+TYPES_PREFIX = 'type='
+MARKET_PREFIX = 'market='
+LIMIT_PREFIX = 'limit='
+OFFSET_PREFIX = 'offset='
+
+
+def search_endpoint(keywords:str, types:list, filters:dict, market:str, limit:int, offset:int):
     
-    # FORMAT QUERRY ITEMS AND FILTERS
-    querry_items = keywords.split(' ')   
+    endpoint = 'https://api.spotify.com/v1/search?'
+    querry_keywords = keywords.split() 
+    
     for filter, value in filters.items():
         value = value.replace(' ', '%20')
-        item = f'{filter}:{value}'
-        querry_items.append(item)
+        querry_keywords.append('{0}:{1}'.format(filter, value))
+    
+    querry_str = QUERRY_PREFIX + '%20'.join(querry_keywords)  
+    market_str = MARKET_PREFIX + str(market)
+    offset_str = OFFSET_PREFIX + str(offset)
+    types_str = TYPES_PREFIX + ','.join(types)
+    limit_str = LIMIT_PREFIX + str(limit)
 
-    # REQUIRED ARGUMENTS
-    querry = 'q=' + '%20'.join(querry_items)
-    types = 'type=' + ','.join(allowed_types)
-    arguments = [querry, types]
+    url_arguments = [
+        querry_str, 
+        market_str, 
+        offset_str,
+        types_str, 
+        limit_str 
+    ]    
     
-    # OPTIONAL ARGUMENTS
-    if market: 
-        arguments.append(f'market={market}')
-    if limit: 
-        arguments.append(f'limit={limit}')  
-    if offset: 
-        arguments.append(f'offset={offset}')
+    url = endpoint + '&'.join(url_arguments)
     
-    return endpoint + '&'.join(arguments)
+    return url
